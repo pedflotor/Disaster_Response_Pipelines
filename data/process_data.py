fa60b98e325b41699120087ea93f589e164a4df8
pedflotor <petanth91@gmail.com>
2021-01-26 10:24:22 +0100

    ETL pipeline
    ETL pipeline used to clean the datasets (disaster_messages.csv and disaster_categories.csv files)

---
+++ data/process_data.py
@@ -0,0 +1,118 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    Function to load the necessary data from 2 files and merge them into one dataframe by "id" column
+
+    Parameters
+    ----------
+    messages_filepath: str
+        path of the messages file
+    categories_filepath: str
+        path of the categories file
+
+    Returns
+    -------
+    df:object
+        dataframe with the merged data by "id" column
+    """
+
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on=["id"])
+    return df
+
+
+def clean_data(df):
+    """
+    Function to clean the df dataframe.
+
+    Parameters
+    ----------
+    df:object
+        dataframe containing the data from the files "messages" and "categories" in raw format ()
+
+    Returns
+    -------
+    dx:object
+        dataframe with the cleaned data
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(";", expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.head(1)
+
+    # use this row to extract a list of new column names for categories
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :].tolist()
+
+    # rename the columns of "categories"
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from "df"
+    df.drop("categories", axis=1, inplace=True)
+
+    # concatenate the original dataframe with the new "categories" dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    dx = df.drop_duplicates(subset=['id'])
+
+    return dx
+
+
+def save_data(df, database_filename):
+    """
+    Function to save the clean dataset into an sqlite database
+
+    Parameters
+    ----------
+    df:object
+        dataframe containing the data already cleaned with the clean_data() function
+    database_filename:str
+        name of the database that will be created
+    """
+
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('database_filename', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+
+        print('Cleaned data saved to database!')
+
+    else:
+        print('Please provide the filepaths of the messages and categories ' \
+              'datasets as the first and second argument respectively, as ' \
+              'well as the filepath of the database to save the cleaned data ' \
+              'to as the third argument. \n\nExample: python process_data.py ' \
+              'disaster_messages.csv disaster_categories.csv ' \
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
